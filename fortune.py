e4a204498d0d111b517fd2cc163fa7c8e5f3db4d
Tykay99 <Tyzkay@Icloud.com>
2022-12-05 15:39:18 -0500

    Outline

--- fortune.py
+++ fortune.py
@@ -1,1 +1,19 @@
+# Writing a program that computes Fortune's Algorithm
 
+# Given a text file, store list of points
+
+# Create a point class w/ instances x,y and methods
+
+# Create a parabola class w/ directrix, focus
+
+# Computer Fortune Algorithm
+
+# function AddParabola ( point u )
+
+# function RemoveParabola ( Parabola p )
+
+# function CheckCircleEvent(Parabola p)
+
+# Main method ForLoop (SweepLine)
+
+# Graphics package for printing diagram
