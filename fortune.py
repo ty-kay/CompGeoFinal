11045006513f9810cb8c9efef004a28288f8a814
Tykay99 <Tyzkay@Icloud.com>
2022-12-06 13:54:19 -0500

    linkedList

--- fortune.py
+++ fortune.py
@@ -22,6 +22,11 @@
 
 # https://pvigier.github.io/2018/11/18/fortune-algorithm-details.html
 
+# https://jacquesheunis.com/post/fortunes-algorithm/
+
+# https://www.cs.hmc.edu/~mbrubeck/voronoi.html
+
+# http://www.bitbanging.space/posts/voronoi-diagram-with-fortunes-algorithm
 
 import sys
 import math
@@ -55,6 +60,20 @@ class Point(object):
         return Point(x, y)
 
 
+class Parabola(object):
+    def __init__(self, direc, focus):
+        self.direc = direc
+        self.focus = focus
+
+    def direc(self):
+      # y = y_0
+        return self.direc
+
+    def focus(self):
+      # x, y point
+        return self.focus
+
+
 def picture(a, b):
     qwer = a
     qwer.append(a[0])
@@ -76,21 +95,34 @@ def isSiteEvent(x):
     return True
 
 
-def AddParabola(u):
-    # par = arc under point u;
-    par = u
-    # if (par has its circle event, when it is removed form the beachline)
+class Node:
+    def __init__(self, direct):
+        self.direct = direct
+        self.nextArc = None
+        self.prevArc = None
+        self.rightEdge = None
+        self.leftEdge = None
 
-    #    remove this event form the queue
-    # new arcs a, b, c;
-    # b.site = u;
-    # a.site = c.site = par.site; // site of arc is a focus of arc
-    # xl, xr  = left and right edges, which comes from point on par under u
-    # xl is a normal to  (a.site, b.site);
-    # xr is a normal to (b.site, c.site);
-    # replace par by the sequence a, xl, b, xr, c
-    # CheckCircleEvent(a);
-    # CheckCircleEvent(c);
+
+class doubly_linked_list:
+    def __init__(self):
+        self.head = None
+
+    def insert_in_emptylist(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+
+
+# def AddParabola(u):
+#     # Create a new arc p with the new site as a focus;
+#     p = Parabola(u)
+
+#     # Find where this arc should be located in the beach line. This is given by the x coordinate of the site identifying an arc q as described above;
+#     # Add p to the beach line as described above splitting the parabola q in ql and qr;
+#     # Delete any vertex event related to the arc q;
+#     # Add vertex events for ql and qr (described later);
+#     # Create a new Voronoi edge.
 
 
 def main():
@@ -99,19 +131,35 @@ def main():
         arr += l.strip().split()
     points = []
     q = PriorityQueue()
+    s = set()
     for j in range(len(arr)):
         if j % 2 == 0:
           # TUPLE
           # (sort by y unless they're identical, in which case sort by x)
             p = (float(arr[j]), float(arr[j + 1]))
             points.append(p)
+            s.add(p)
     points = Sort_Tuple(points)
+    # add a site event in the event queue for each site
     for p in points:
         q.put((p[1], p))
+    # while the event queue is not empty
+    linkedList = doubly_linked_list()
     while not q.empty():
-        e = q.get()
-        if isSiteEvent(e):
-            AddParabola(e)
+        # pop the top event
+        e = q.get()[1]
+        print(e)
+        # if the event is a site event
+        if e in s:
+            print("in")
+            # insert a new arc in the beachline
+            # check for new circle events
+        else:
+            print("out")
+            # create a vertex in the diagram
+            # remove the shrunk arc from the beachline
+            # delete invalidated events
+            # check for new circle events
 
 
 if __name__ == "__main__":
