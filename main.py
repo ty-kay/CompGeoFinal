d32b9f371f3047f23801f963950500a57a08e4c5
Tykay99 <Tyzkay@Icloud.com>
2022-12-11 14:50:42 -0500

    outline

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+import Fortune6
+import matplotlib.pyplot as plt
+
+
+def main():
+    arr = []
+    for l in sys.stdin:
+        arr += l.strip().split()
+    points = []
+    for j in range(len(arr)):
+        if j % 2 == 0:
+            p = (float(arr[j]), float(arr[j + 1]))
+            points.append(p)
+    # Ty: Sorts by X coordinate in case same Y coordinate
+    points = sorted(points, key=lambda x: x[1])
+    for point in points:
+        plt.plot(point[0], point[1], 'o')
+    qwer = Fortune6.Voronoi(points)
+    qwer.process()
+    plt.xlim([-10, 10])
+    plt.ylim([-10, 10])
+    for a in qwer.get_output():
+        x_values = [a[0], a[2]]
+        y_values = [a[1], a[3]]
+        plt.plot(x_values, y_values, 'bo', linestyle="--")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
